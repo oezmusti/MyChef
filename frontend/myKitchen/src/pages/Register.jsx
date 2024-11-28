dcb51f397de10b14ed8cdc2da7db9a607db6c06f
Mweynands00 <maximilian.weynands@iu-study.org>
2024-11-28 10:18:32 +0100

    Farben Aktuell

--- frontend/myKitchen/src/pages/Register.jsx
+++ frontend/myKitchen/src/pages/Register.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import Header from '../layout/header';
-import Footer from '../layout/footer';
 
 function Register() {
     return (
         <>
-            <div className=''>
-                Login und Register Formular
+     <section className="bg-gold-50 dark:bg-gold-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gold-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="" alt="logo"/>
+                    MyChef 
+                </a>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gold-800 dark:border-gold-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gold-950 md:text-2xl dark:text-white">
+                            Wilkommen @username
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gold-950 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-Patt1-Tan border border-gold-300 text-gold-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gold-700 dark:border-gold-600 dark:placeholder-gold-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gold-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gold-50 border border-gold-300 text-gold-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gold-700 dark:border-gold-600 dark:placeholder-gold-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <p className="text-sm font-light text-gold-950 dark:text-gold-950">
+                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
             </div>
+        </section>
+
         </>
     );
 }
+
 
 export default Register; 
