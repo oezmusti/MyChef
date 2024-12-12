62dbcefd7093075ccc4b1b61217c3ee98f83eb8e
SammyFFM <80925861+SammyFFM@users.noreply.github.com>
2024-12-12 12:41:13 +0100

    AuthController for user-login added

--- backend/src/main/java/com/example/myRezept_Backend/model/User.java
+++ backend/src/main/java/com/example/myRezept_Backend/model/User.java
@@ -13,4 +13,44 @@ public class User {
     private String role = "USER"; // Standardrolle
 
     // Getter und Setter
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
