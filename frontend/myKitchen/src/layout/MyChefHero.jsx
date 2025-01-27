de1ff718f84f1ad2a049c800f3cd0dda3b662abc
Mustafa Öz <mustafa2002oezmail.com>
2025-01-27 18:42:07 +0100

    Verbesserung im Forntend

--- frontend/myKitchen/src/layout/MyChefHero.jsx
+++ frontend/myKitchen/src/layout/MyChefHero.jsx
@@ -13,8 +13,18 @@ function MyChefHero() {
                                 <img src="\MyChef_Transparent_Logo.png" alt="" className="MC-img-inner" />
                             </div>
                         </div>
-                        <div className='MC-eight-side'>
-
+                        <div className='MC-reight-side'>
+                            <div>
+                                <div>
+                                    <h1 className='hl-1'> Willkommen bei <span className='hl-accente'> MyChef </span></h1>
+                                    <h2 className='hl-2'> Hier bist <span className='hl-accente'> DU </span> der <span className='hl-accente'> Chef </span> </h2>
+                                </div>
+                                <div className='primary-button'>
+                                    <a href="/register" className='button primary-buton'>
+                                        Jetzt Regisstrieren
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
